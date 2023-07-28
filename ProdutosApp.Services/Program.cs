53f4fada7decd0971b2a65f06e2698764205855d
LucasMoraesDev <lms.262@gmail.com>
2023-07-28 10:53:31 -0300

    feat: Desenvolvimento da API, configurações para publicação

--- ProdutosApp.Services/Program.cs
+++ ProdutosApp.Services/Program.cs
@@ -7,17 +7,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//criando a política de CORS do projeto
+builder.Services.AddCors(
+          config => config.AddPolicy("DefaultPolicy", builder =>
+          {
+              builder.AllowAnyOrigin() //qualquer dominio poderá acessar a API
+                     .AllowAnyMethod() //permitir POST, PUT, DELETE, GET
+                     .AllowAnyHeader(); //aceitar parametros de cabeçalho de requisição
+          })
+          );
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
+
+//utilizando a política de CORS
+app.UseCors("DefaultPolicy");
 
 app.MapControllers();
 
 app.Run();
+
+
+
